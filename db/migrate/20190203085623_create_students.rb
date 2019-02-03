8672da2c67174c74875b0a7b65fa0a830fc03d60
Rit-zz <ritik.poshattiwar@gmail.com>
2019-02-03 14:45:40 +0530

    initial commit

---
+++ db/migrate/20190203085623_create_students.rb
@@ -0,0 +1,16 @@
+class CreateStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+    	t.integer :roll
+    	t.text :name
+    	t.integer :mobile
+    	t.string :branch
+    	t.integer :year
+    	t.date :d_o_b
+    	t.string :gender
+    	t.string :city
+    	t.string :password
+      	t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20190203090638_courses.rb
@@ -0,0 +1,10 @@
+class Courses < ActiveRecord::Migration[5.2]
+  def change
+  	create table :courses do |t|
+  		t.integer :c_id
+  		t.text :c_name
+  		t.integer :duration
+  		t.integer :fees
+  		t.timestamps
+  end
+end
